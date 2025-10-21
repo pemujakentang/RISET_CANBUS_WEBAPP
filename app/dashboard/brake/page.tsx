9bb381c949b308983a0abe774b6e0d0e9814d55e
Bonifasius Martin <martinwibawa@gmail.com>
2025-10-21 10:19:55 +0700

    Minor adjustments

--- app/dashboard/brake/page.tsx
+++ app/dashboard/brake/page.tsx
@@ -1,5 +1,5 @@
 import ParameterDetail from "@/app/components/ParameterDetail";
 
 export default function BrakePage() {
-    return <ParameterDetail name="Brake Pressure" unit="bar" color="#dc2626" parameterKey="brake" />;
+    return <ParameterDetail name="Brake Pressure" unit="%" color="#dc2626" parameterKey="brake" />;
 }
